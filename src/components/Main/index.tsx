0c2979fae2bacacf8cfcae41b5909ec738d0415b
robson <robson1992ferreira@hotmail.com>
2021-09-22 20:56:57 -0300

    create boilerplate

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = ({title = 'React Avançado', description= 'Typescript, React.js Next.js e Styled Components'}) => (
+  <S.Wrapper>
+    <S.Logo src="img/logo.svg"  alt="Imagem de um átomo e React Avançado escrito ao lado"/>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="img/hero-illustration.svg" alt="Um desenvolvedor em frente a tela do codigo"/>
+  </S.Wrapper>
+)
+
+export default Main
