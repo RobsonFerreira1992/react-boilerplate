c8107438f0bb0857249613552d6c7e1fadff3812
robson <robson1992ferreira@hotmail.com>
2022-01-28 11:47:46 -0300

    update

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,18 @@
-import GlobalStyles from "../src/styles/global";
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider
+  }
+}
 
 export const decorators = [
-  (Story) =>(
-    <>
-      <GlobalStyles/>
-      <Story/>
-    </>
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
   )
 ]
